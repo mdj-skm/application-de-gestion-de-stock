d0dc39d595bf0e1f9446570443b6f83f8edaa64c
bouattara-dev <Ouattarabini225@gmail.com>
2025-05-26 15:44:38 +0200

    base fournisseurs

---
+++ backend_stock/stock/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Fournisseur(models.Model):
+    nom = models.CharField(max_length=100)
+    entreprise = models.CharField(max_length=100)
+    produit = models.CharField(max_length=100)
+    quantite = models.IntegerField()
+    email = models.EmailField()
+    numero = models.CharField(max_length=20)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.nom
